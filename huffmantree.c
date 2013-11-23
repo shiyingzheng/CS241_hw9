fe3c348329400d76191c8c4c02cbe7b95f93df2a
SHIYING <szheng@oberlin.edu>
2013-11-23 16:41:29 -0500

    huffmantree methods

--- huffmantree.c
+++ huffmantree.c
@@ -1,7 +1,45 @@
 #include "huffmantree.h"
-huffmantree* huffmantree_init();
-huffmantree* huffmantree_init(char* string);
-char* huffmantree_tostring();
-int huffmantree_isempty(huffmantree* tree);
-int huffmantree_isleaf(huffmantree* tree);
-void huffmantree_free(huffmantree* tree);
+#define NOCHAR -2;
+void error(){
+	exit(3);
+}
+huffmantree* huffmantree_init(){
+	huffmantree* tree=malloc(sizeof(huffmantree));
+	if(!tree) error();
+	tree->c=NOCHAR;
+	tree->count=0;
+	tree->left=NULL;
+	tree->right=NULL;
+	return tree;
+}
+huffmantree* huffmantree_init_from_string(char* string){
+	huffmantree* tree=malloc(sizeof(huffmantree));
+	if(!tree) error();
+	tree->c=string;
+	tree->count=0;
+	tree->left=NULL;
+	tree->right=NULL;
+	return tree;
+}
+char* huffmantree_tostring(huffmantree* tree){
+
+}
+int huffmantree_isempty(huffmantree* tree){
+	if(!tree->count) return 1;
+	return 0;
+}
+int huffmantree_isleaf(huffmantree* tree){
+	if(!tree->left&&!tree->right) return 1;
+	return 0;
+}
+void huffmantree_free(huffmantree* tree){
+	if (tree->right){
+		huffmantree_free(tree->right);
+		free(tree);
+	}
+	if (tree->left){
+		huffmantree_free(tree->left);
+		free(tree);
+	}
+	free(tree);
+}
--- huffmantree.h
+++ huffmantree.h
@@ -4,13 +4,13 @@
  * A definition for a huffman tree
  */
 typedef struct huffmantree{
-	char c;
+	int c;
 	int count;
 	huffmantree* left;
 	huffmantree* right;
 } huffmantree;
 extern huffmantree* huffmantree_init();
-extern huffmantree* huffmantree_init(char* string);
+extern huffmantree* huffmantree_init_from_string(char* string);
 extern char* huffmantree_tostring();
 extern int huffmantree_isempty(huffmantree* tree);
 extern int huffmantree_isleaf(huffmantree* tree);
