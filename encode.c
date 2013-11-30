9ca7f79decf7a50766a0c5501c34f52d86543176
SHIYING <szheng@oberlin.edu>
2013-11-30 17:36:02 -0500

    encode finished

--- encode.c
+++ encode.c
@@ -79,43 +79,49 @@ char* mystrdup(char* string,int size){
 		i++;
 	}
 	newstr[i]=0;
-	//printf("%s %s\n",string,newstr);
+	//printf("%s %s %d\n",string,newstr,size);
 	return newstr;
 }
 void filltable(huffmantree* tree, char** array, char* path){
 	if (huffmantree_isleaf(tree)){
 		int c=tree->c;
 		//printf("This is the number in the tree %d\n",c);
-		array[c]=path;
+		//array[c]=mystrdup(path,strlen(path)+1);
+		memcpy(array[c],path,strlen(path)+1);
 	}
 	else{
 		char* left_path=mystrdup(path,strlen(path)+2);
 		char* right_path=mystrdup(path, strlen(path)+2);
-		//printf("%d %d\n",strlen(path),strlen(left_path));
 		strcat(left_path,"0");
 		strcat(right_path, "1");
+		//printf("%lu %lu\n",strlen(path),strlen(left_path));
+		//printf("%s %s\n", left_path, right_path);
+		//printf("%p %p, %p\n",path, left_path, right_path);
 		filltable(tree->left, array, left_path);
 		filltable(tree->right, array, right_path);
 		free(left_path);
 		free(right_path);
+		//printf("freed %p, %p\n",left_path, right_path);
 	}
 }
 
 void fill(huffmantree* tree, char** array){
-	int slot_size=((tree->size-1)/2+1)*sizeof(char); 
+	int slot_size=((tree->size-1)/2+1)*sizeof(char); //maximum length of a path from root to leaf
 	char* path=malloc(slot_size);
 	path[0]=0;
 	filltable(tree, array, path);
+	free(path);
 }
 void free_table(char** t){
-	for(int i=0;i<=CHAR_BIT;++i){
+	for(int i=0;i<pow(2,CHAR_BIT)+1;++i){
 		free(t[i]);
 	}
 	free(t);
 }
 char** table(huffmantree* tree){
 	int size=(int)pow(2,CHAR_BIT)+1;
-	int slot_size=((tree->size-1)/2+1)*sizeof(char); 
+	int slot_size=((tree->size-1)/2+1)*sizeof(char); //maximum length of a path from root to leaf
+	//printf("Slot size is %d\n",slot_size);
 	char** array=malloc(sizeof(char*)*size);
 	for(int i=0;i<size;++i){
 		array[i]=malloc(slot_size);
@@ -167,21 +173,59 @@ linkedlist* bits(huffmantree* tree, FILE* f){
 		i++;
 	}
 	free_table(t);
+	free(treestring);
 	return(list);
+}
+char* list_to_string(linkedlist* list){
+	int remain=CHAR_BIT-list->size%CHAR_BIT;
+	int binary_size=list->size+remain;
+	char* string=malloc(sizeof(char)*(binary_size/CHAR_BIT+1));
+	int counter=0;
+	int sum=0;
+	int i=0;
+	iterator* iter=linkedlist_iterator(list);
+	string[0]=0;
+	while (counter<binary_size){
+		if (linkedlist_iteratorhasnext(iter)){
+			sum=sum*2+(*(char*)linkedlist_iteratornext(iter)-'0');
+		}
+		else{
+			sum=sum*2;
+		}
+		counter++;
+		if (counter%CHAR_BIT==0){
+			string[i]=sum;
+			//printf("%d %c\n",sum, sum);
+			i++;
+			sum=0;
+		}
+	}
+	string[i]=0;
+	linkedlist_freeiter(iter);
+	return string;
 }
 int main(int argc, char *argv[]){
 	FILE * f=fopen("meow","r");
 	huffmantree* tree=frequency(f);
 	rewind(f);
 	linkedlist* list=bits(tree,f);
-	iterator* iter=linkedlist_iterator(list);
+	char* string=list_to_string(list);
+	printf("%s\n", string);
+	int i=0;
+	/*while(string[i]){
+		printf("%d %c\n",string[i],string[i]);
+		i++;
+	}*/
+	free(string);
+	/*iterator* iter=linkedlist_iterator(list);
 	while(linkedlist_iteratorhasnext(iter)){
 		printf("%c",*(char*)linkedlist_iteratornext(iter));
 	}
 	printf("\n");
+	linkedlist_freeiter(iter);*/
 	huffmantree_free(tree);
 	linkedlist_free(list);
-	linkedlist_freeiter(iter);
+	
 	fclose(f);
 	/*char* line=malloc(sizeof(char)*LINE_MAX);
 	getline(&line,LINE_MAX,f);
